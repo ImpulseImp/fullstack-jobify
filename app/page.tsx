22d4ce6be76feaed68cef2dd9f7792d994d512d5
Impulse <impulses4mini@gmail.com>
2024-06-01 21:12:26 +0300

    add HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -10,22 +10,28 @@ const HomePage = () => {
       <header className="mx-auto max-w-6xl px-4 py-6 sm:px-8">
         <Image src={Logo} alt="logo" />
       </header>
-      <section className="mx-auto -mt-20 grid h-screen max-w-6xl items-center px-4 sm:px-8 lg:grid-cols-[1fr,400px]">
+      <section className="mx-auto -mt-20 grid h-screen items-center px-4 py-6 sm:px-8 lg:grid-cols-[1fr,400px]">
         <div>
           <h1 className="text-4xl font-bold capitalize md:text-7xl">
-            job <span className="text-primary">tracking</span> app
+            Job <span className="text-primary">tracking</span> app
           </h1>
           <p className="mt-4 max-w-md leading-loose">
-            I am baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe,
+            delectus! Rem labore incidunt delectus laboriosam eos unde numquam!
+            Laudantium et voluptate sint tempore, corrupti esse. Perferendis hic
+            iste quod accusantium?
           </p>
           <Button asChild className="mt-4">
             <Link href="/add-job">Get Started</Link>
           </Button>
         </div>
-        <Image src={LandingImg} alt="landing" className="hidden lg:block" />
+        <div>
+          <Image
+            src={LandingImg}
+            alt="landing svg"
+            className="hidden lg:block"
+          />
+        </div>
       </section>
     </main>
   );
