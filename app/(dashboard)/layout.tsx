13bcad0157195ea9c709fee3cd52a6f241de4b6d
Impulse <impulses4mini@gmail.com>
2024-06-01 22:40:13 +0300

    add Navbar and Sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,20 @@
 import { PropsWithChildren } from "react";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const layout = ({ children }: PropsWithChildren) => {
-  return <div>{children}</div>;
+  return (
+    <main className="grid lg:grid-cols-5">
+      {/* first col hide on small screen */}
+      <div className="hidden lg:col-span-1 lg:block lg:min-h-screen">
+        <Sidebar />
+      </div>
+      {/* second-col hide dropdown on big screen*/}
+      <div className="lg:col-span-4">
+        <Navbar />
+        <div className="px-4 py-16 sm:px-8 lg:px-16">{children}</div>
+      </div>
+    </main>
+  );
 };
 export default layout;
---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
+import LinksDropDown from "./LinksDropDown";
+import ThemeToggle from "./ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <nav className="lf:px-24 flex items-center justify-between bg-muted px-4 py-4 sm:px-16">
+      <div>
+        <LinksDropDown />
+      </div>
+
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton />
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
