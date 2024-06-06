9a7a50acbc529f46b3346ef1a69dd140ebaea40f
Impulse <impulses4mini@gmail.com>
2024-06-06 17:07:59 +0300

    complete stats

--- app/(dashboard)/jobs/loading.tsx
+++
@@ -1,4 +0,0 @@
-function loading() {
-  return <h2 className="text-xl font-medium capitalize">loading...</h2>;
-}
-export default loading;
---
+++ components/StatsCard.tsx
@@ -0,0 +1,44 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Skeleton } from "./ui/skeleton";
+
+type StatsCardsProps = {
+  title: string;
+  value: number;
+};
+
+function StatsCards({ title, value }: StatsCardsProps) {
+  return (
+    <Card className="bg-muted">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <CardTitle className="capitalize">{title}</CardTitle>
+        <CardDescription className="mt-[0px!important] text-4xl font-extrabold text-primary">
+          {value}
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
+
+export function StatsLoadingCard() {
+  return (
+    <Card className="h-[88px] w-[330px]">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <div className="flex items-center space-x-4">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[150px]" />
+            <Skeleton className="h-4 w-[100px]" />
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  );
+}
+
+export default StatsCards;
